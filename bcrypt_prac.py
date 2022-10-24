4b0d2aac1d8c5350997c0dd0728c03b68978edfe
barioder <barioder27@gmail.com>
2022-10-24 15:09:37 +0300

    Work Factor

--- bcrypt_prac.py
+++ bcrypt_prac.py
@@ -11,3 +11,24 @@ if bcrypt.checkpw(password, hashed):
     print("It is a match")
 else:
     print("It didnt match fam")
+
+# work factor implication on the hash 
+print("work factor implication on the hash ")
+import time
+
+start = time.time()
+hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+end = time.time()
+
+f = end - start
+
+print(f)
+
+# work factor can be increased by passing a round argument
+# Note the difference in the 2 
+
+start = time.time()
+hashed = bcrypt.hashpw(password, bcrypt.gensalt(rounds=15))
+end = time.time()
+
+print(end - start)
